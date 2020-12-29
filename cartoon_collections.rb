7d346fa3049e669267effdacf8e826606cd662ea
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-29 00:28:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,20 @@
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
+  newArray = Array.new
+  
+  array.map do |element|
+    newArray << element * element
+  end
+  newArray
 end
 
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+  newArray = Array.new
+  planeteer_calls.map do |element|
+    newArray << element.up
 end
 
 def long_planeteer_calls(planeteer_calls)
